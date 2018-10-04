643ed131be93814e7411a8cd2187ea5533a99abe
Sidde Gowda <engowdre@gmail.com>
2018-10-04 12:08:27 -0700

    C programming of utility and test functions

--- programming/check_power_2.c
+++ programming/check_power_2.c
@@ -1,13 +1,26 @@
 #include <stdio.h>
 
+int check_power (unsigned int n)
+{
+    while (n != 1) {
+        if (n % 2 != 0) {
+            return (0);
+        }
+
+        n = n/2;
+    }
+    
+    return (1);
+}
+
 int main (void)
 {
-    int n;
+    unsigned int n;
 
     printf ("enter num: ");
     scanf ("%d", &n);
 
-    printf("given num %s power of 2\n", (n && (n-1) == 0) ?"is":"is not");
+    printf("given num %s power of 2\n", check_power(n) ?"is":"is not");
 
     return (0);
 }
