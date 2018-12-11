2e3d078c207d39657ff7bd0bcec02316579fab4a
universini <engowdre@gmail.com>
2018-12-11 04:46:49 +0000

    Added a new file

--- datastructures/slist_2.c
+++ datastructures/slist_2.c
@@ -21,6 +21,51 @@ void print_list (node *n)
     return;
 }
 
+int list_len(node *l)
+{
+    int c = 0;
+
+    while (l->next) {
+        c++;
+        l = l->next;
+    }
+
+    return(c);
+}
+
+node *find_merge_point_node(node *n1, node *n2, int ld)
+{
+    for (int i=0; i<ld; i++) {
+        if (!n1)
+            return (NULL);
+        n1 = n1->next;
+    }
+
+    while (n1 && n2) {
+        if (n1 == n2)
+            return (n1);
+        n1 = n1->next;
+        n2 = n2->next;
+    }
+
+    return (NULL);
+}
+
+node *find_merge_point(node *n1, node *n2)
+{
+    int l1 = list_len(n1);
+    int l2 = list_len(n2);
+    int ld;
+
+    if (l1 > l2) {
+        ld = l1 - l2;
+        return (find_merge_point_node(n1, n2, ld));
+    } else {
+        ld = l2 - l1;
+        return (find_merge_point_node(n2, n1, ld));
+    }
+}
+
 node *split_half (node *s)
 {
     node *fast = s;
@@ -77,6 +122,9 @@ int main (void)
     printf ("split linked lists: \n");
     print_list (n);
     print_list (s);
+
+	s = find_merge_point(n, s);
+	if (s) printf("Merge Point: %d\n", s->num);
 
     return (0);
 }
