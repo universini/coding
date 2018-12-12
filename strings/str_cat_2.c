2f77e036da11c7872b704d694a33de2e86a1ae2f
universini <engowdre@gmail.com>
2018-12-12 23:15:45 +0000

    Added new files

---
+++ strings/str_cat_2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+
+int main (void)
+{
+	char s1[32] = "sidde ";
+	char s2[] = "gowda";
+
+	strcpy(s1+strlen(s1), s2);
+	printf("new string: %s\n", s1);
+
+	return(0);
+}
+
