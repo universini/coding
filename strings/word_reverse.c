f7b94fda69b9ed3ee91b11050b0ff44c653f1cf5
universini <engowdre@gmail.com>
2018-12-13 22:44:30 +0000

    Added a new file

---
+++ strings/word_reverse.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<string.h>
+
+void word_reverse(char s[])
+{
+	int l = strlen(s) - 1;
+
+	while(l--) {
+		if (*(s+l) == ' ') {
+			*(s+l) = '\0';
+			printf("%s ", &(*(s+l+1)));
+		}
+	}
+
+	printf("%s\n", s);
+
+	return;
+}
+
+int main(void)
+{
+	char s[] = "sidde gowda nidagatta";
+
+	word_reverse(s);
+
+	return(0);
+}
