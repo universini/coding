27b077252cfa483a47ed60752da499c9cd73193a
universini <engowdre@gmail.com>
2020-04-17 19:46:25 +0000

    Added new files

---
+++ arrays/find_single.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+
+/* In a non-empty array every element appears twice except one. Find it */
+
+int find_single(int a[], int size)
+{
+	int num = a[0];
+
+	for (int i=1; i<size; i++)
+	{
+		 num = num ^ a[i];
+	}
+
+	return(num);
+}
+
+int main(void)
+{
+	int a[] = {2,2,3,3,4,4,5,6,6};
+
+	printf("num: %d\n", find_single(a, 9));
+	return(0);
+}
+
+/*
+00
+11
+
+11
+11
+
+000
+100
+
+100
+100
+
+000
+101
+
+101
+110
+
+011
+110
+
+101 ->5
+*/
