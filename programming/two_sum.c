3b9b5e66236343296aaec980f5b1c030f847018b
universini <engowdre@gmail.com>
2020-05-29 04:39:45 +0000

    Added a new file

---
+++ programming/two_sum.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int two_sum(int a[], int num, int len)
+{
+	int start = 0;
+	int end = len -1;
+	int sum = 0;
+
+	while (start < end)
+	{
+		sum = a[start]+a[end];
+		if (sum == num) return(1);
+		else if (sum < num) start++;
+		else end--;
+	}
+
+	return(0);
+}
+
+int main(void)
+{
+	int a[] = {1,2,4,6,7,9};
+	int len = sizeof(a)/sizeof(a[0]);
+
+	printf("%s\n", two_sum(a,13,len) ? "True" : "False");
+	return(0);
+}
+
+
