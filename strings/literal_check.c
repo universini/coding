e990aadbf7cba811cd359264f188096ade443e16
universini <35048861+universini@users.noreply.github.com>
2020-10-01 14:16:19 -0700

    String literal check

---
+++ strings/literal_check.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+
+int main (void)
+{
+    char *s1 = "Sidde";
+    char *t1;
+    char  s2[] = "Gowda";
+
+    printf ("s1: %s s2: %s\n", s1, s2);
+
+    t1 = s1;
+    s2[3] = 'r';
+
+    printf ("t1: %s s2: %s\n", t1, s2);
+
+    t1 = s2;
+
+    printf ("t1: %s\n", t1);
+
+    *(s1+4) = 'u';
+
+    printf ("s1: %s\n", s1);
+
+    return (0);
+}
