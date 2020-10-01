403d53de60a136a6b80d7495b0fe9955eecceefa
universini <engowdre@gmail.com>
2020-10-01 21:49:34 +0000

    Updated str_len functions

---
+++ strings/str_len.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+int str_len_1(const char *s)
+{
+	const char *t = s;
+
+	while(*++s);
+
+	return(s-t);
+}
+
+int str_len_2(const char *s)
+{
+	int i=0;
+
+	while(s[i++]);
+
+	return(--i);
+}
+
+int main(void)
+{
+	char s[]="Sidde Gowda";
+
+	printf("String len method1: %d\n", str_len_1(s));
+	printf("String len method2: %d\n", str_len_2(s));
+
+	return(0);
+}
+
