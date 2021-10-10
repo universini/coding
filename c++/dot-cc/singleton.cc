5a3b4147db510a1bf3bf4288e0922f745b80f2aa
universini <engowdre@gmail.com>
2021-10-10 19:39:26 +0000

    Added singleton example

---
+++ c++/dot-cc/singleton.cc
@@ -0,0 +1,48 @@
+#include <iostream>
+
+namespace singleton {
+class ston {
+  static ston* inst;
+  std::string name;
+
+ public:
+  static ston* get_instance();
+  void set_name(std::string n) { name = n; }
+  std::string get_name(void) { return name; }
+};
+}
+
+singleton::ston* singleton::ston::inst = nullptr;
+
+singleton::ston* singleton::ston::get_instance() {
+  if (inst == nullptr) {
+    inst = new singleton::ston();
+  }
+
+  return inst;
+}
+
+void some_func(void) {
+  std::string name{"Nidagatta"};
+  singleton::ston* s = s->get_instance();
+  if (s == nullptr) {
+    std::cout << "singleton is nullptr" << std::endl;
+  } else {
+    s->set_name(name);
+    std::cout << "singleton data : " << s->get_name() << std::endl;
+  }
+}
+
+int main(void) {
+  std::string name{"Sidde Gowda"};
+  singleton::ston* s = s->get_instance();
+  if (s == nullptr) {
+    std::cout << "singleton is nullptr" << std::endl;
+  } else {
+    s->set_name(name);
+    some_func();
+    std::cout << "singleton data : " << s->get_name() << std::endl;
+  }
+
+  return 0;
+}
