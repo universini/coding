8372ff038f76ed968b3a295cc60921d78357f5af
Sidde Gowda <engowdre@gmail.com>
2023-05-28 14:21:17 -0700

    Added aligned malloc function

---
+++ c/programming/malloc_align.c
@@ -0,0 +1,25 @@
+#include <malloc.h>
+#include <stdio.h>
+
+#define ALIGN 64
+
+void *aligned_malloc(int size) {
+  void *mem = malloc(size + ALIGN + sizeof(void *));
+  void **ptr = (void **)((long)(mem + ALIGN + sizeof(void *)) & ~(ALIGN - 1));
+  ptr[-1] = mem;
+  return ptr;
+}
+
+void aligned_free(void *ptr) { free(((void **)ptr)[-1]); }
+
+int main(void) {
+  int *p;
+
+  p = aligned_malloc(0);
+
+  printf("size: %lu content: %d\n", sizeof(p), *p);
+
+  aligned_free(p);
+
+  return (0);
+}
