3f922c6411b6477cf44528021032c3e83df177cd
root <root@DESKTOP-QU7DEEK.localdomain>
2023-11-27 13:05:59 -0800

    updated str_cmp file

--- c/string/str_cmp.c
+++ c/string/str_cmp.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 
-int str_cmp(char *s1, char *s2) {
+int str_cmp1(char *s1, char *s2) {
   while (*s1 == *s2)
     if (*s1 == '\0' || *s2 == '\0')
       return (0);
@@ -13,11 +13,24 @@ int str_cmp(char *s1, char *s2) {
   return (*s1 - *s2);
 }
 
+/*
+ * Compare strings:  s1>s2: >0  s1==s2: 0  s1<s2: <0
+ */
+int str_cmp2(char *s1, char *s2) {
+  while (*s1 == *s2++)
+    if (*s1++=='\0')
+      return(0);
+  return(*s1 - *--s2);
+}
+
 int main(void) {
   char s[] = "sidde";
   char t[] = "siddego";
 
-  printf("str_cmp returned: %d\n", str_cmp(s, t));
+  printf("str_cmp1 returned: %s\n",
+    str_cmp1(s, t) == 0 ? "pass" : "fail");
+  printf("str_cmp2 returned: %s\n",
+    str_cmp2(s, t) == 0 ? "pass" : "fail");
 
   return (0);
 }
