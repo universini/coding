7d29710d8aa0f05bedede4fb70d57ecd8c2c4b45
Sidde Gowda <engowdre@gmail.com>
2024-02-06 12:17:45 -0800

    added a new file

--- c++/programming/insert_new_num_into_array.cpp
+++ c++/programming/insert_new_num_into_array.cpp
@@ -5,9 +5,9 @@
 int main() {
     std::vector<int> myVector = {10, 20, 30, 40, 50};
     
-    int newNumber = 25; // The integer you want to insert
-    int lowerBound = 20; // Lower bound of the range
-    int upperBound = 30; // Upper bound of the range
+    int newNumber = 15; // The integer you want to insert
+    int lowerBound = 10; // Lower bound of the range
+    int upperBound = 20; // Upper bound of the range
 
     auto it = std::find_if(myVector.begin(), myVector.end(), [lowerBound, upperBound](int num) {
         return num > lowerBound; // && num < upperBound;
