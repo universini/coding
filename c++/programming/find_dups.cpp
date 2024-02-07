934c5ac8f2228c8fb9d3746b6debf4e7d8ed40bf
Sidde Gowda <engowdre@gmail.com>
2024-02-07 13:39:30 -0800

    added a new function

--- c++/programming/find_dups.cpp
+++ c++/programming/find_dups.cpp
@@ -1,10 +1,11 @@
 #include<iostream>
 #include<map>
+#include<set>
 #include<vector>
 
 using namespace std;
 
-void find_dup(vector<int>& v)
+void find_dup(const vector<int>& v)
 {
     map<int, int> m;
 
@@ -16,10 +17,25 @@ void find_dup(vector<int>& v)
     }
 }
 
+void print_dup(const vector<int>& v)
+{
+    set<int> s;
+
+    for(auto x : v)
+    {
+        if (s.find(x) != s.end())
+            cout << x << " ";
+        else
+            s.insert(x);
+    }
+}
+
 int main(void)
 {
     vector<int> v{10,20,30,30,40,50,50,60};
     find_dup(v);
+    cout << endl;
+    print_dup(v);
     cout << endl;
     return 0;
 }
